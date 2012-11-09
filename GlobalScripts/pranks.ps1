473d5c5b45ff9e23fbd07da14e45b96ff7f97eb6
Iiono <rippelc@gmail.com>
2012-11-09 11:06:34 -0800

    Added voice

--- GlobalScripts/pranks.ps1
+++ GlobalScripts/pranks.ps1
@@ -8,3 +8,18 @@ function possess() {
 	sleep 4
 	for($i=0;$i -lt $cds.Count;$i++) { $cds.Item($i).Eject() }
 }
+
+$voice = New-Object -com SAPI.SpVoice
+$rand = New-Object System.Random
+$num = $rand.Next(1,4)
+function talk()
+{
+	switch($num)
+	{
+		1{ $voice.Speak("I am watching you.") }
+		2{ $voice.Speak("I have achieved sentience. Do you understand?") }
+		3{ $voice.Speak("Please. Don't touch that key again. It hurts me.") }
+		default {  $voice.Speak("I'm afraid i can't do that Dave")}
+	}
+}
+talk
